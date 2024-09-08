3851ac0b05c20b58f278b3eee400a0cfd67727b4
Samuel Marchal <samuel.marchal@wakeo.co>
2024-09-08 17:09:04 +0200

    :recycle: Use html template engine

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,13 +1,19 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+
+  // enable HMR with live reload
   devServer: {
-    liveReload: true,
-    hot: true,
-    open: true,
-    static: ['./'],
+    static: path.resolve(__dirname, 'dist'),
+    watchFiles: {
+      paths: ['src/**/*.*'],
+      options: {
+        usePolling: true,
+      },
+    },
   },
 });
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,27 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: 'doc', to: 'doc' },
-        { from: 'img', to: 'img' },
-        { from: 'css', to: 'css' },
-        { from: 'apple-touch-icon.png', to: 'apple-touch-icon.png' },
-        { from: 'favicon-16x16.png', to: 'favicon-16x16.png' },
-        { from: 'favicon-32x32.png', to: 'favicon-32x32.png' },
-        { from: 'favicon.ico', to: 'favicon.ico' },
-        { from: 'favicon.svg', to: 'favicon.svg' },
-        { from: 'robots.txt', to: 'robots.txt' },
-        { from: 'safari-pinned-tab.svg', to: 'safari-pinned-tab.svg' },
-      ],
-    }),
-  ],
 });
